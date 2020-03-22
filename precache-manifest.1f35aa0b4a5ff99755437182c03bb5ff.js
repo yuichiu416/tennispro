8f0aa24f17a5f260c868bc27aec9aaf2d5aac816
Roger Kiew <yuichiu416@gmail.com>
2020-03-22 00:57:23 -0700

    Updates

---
+++ precache-manifest.1f35aa0b4a5ff99755437182c03bb5ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0363e0e3ecb6d6ff62a5944c5c44a7ae",
+    "url": "/tennispro/index.html"
+  },
+  {
+    "revision": "be414becdbf4260fb8b0",
+    "url": "/tennispro/static/css/main.2a99a7c1.chunk.css"
+  },
+  {
+    "revision": "7e4e2f3ac42e4f1e9039",
+    "url": "/tennispro/static/js/2.de4b57da.chunk.js"
+  },
+  {
+    "revision": "90fb4deb912422b947a6490378160cce",
+    "url": "/tennispro/static/js/2.de4b57da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be414becdbf4260fb8b0",
+    "url": "/tennispro/static/js/main.69168c6c.chunk.js"
+  },
+  {
+    "revision": "c4b17494152062dda212",
+    "url": "/tennispro/static/js/runtime-main.4d892bf8.js"
+  }
+]);
